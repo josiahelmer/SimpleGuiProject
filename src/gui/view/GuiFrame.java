970b8c3be2564b4aabbbc57bda12a6fb72f78143
josiahelmer <jelm9149@740s-pl-112.local>
2015-11-09 12:45:01 -0700

    Getting the window to work

--- src/gui/view/GuiFrame.java
+++ src/gui/view/GuiFrame.java
@@ -1,6 +1,23 @@
 package gui.view;
 
-public class GuiFrame
+import javax.swing.JFrame;
+import gui.controller.GuiController;
+
+public class GuiFrame extends JFrame
 {
 
+	private GuiController baseController;
+	
+	public GuiFrame(GuiController baseController)
+	{
+		this.baseController = baseController; //assign paramter to data member
+		setupFrame();
+
+	}
+	private void setupFrame()
+	{
+		this.setResizable(false);	//Usually a good idea but not required
+		this.setSize(400,400);		//Sets the size.
+		this.setVisible(true);		//Must be the last line of code in the setupFrame method.
+	}
 }
